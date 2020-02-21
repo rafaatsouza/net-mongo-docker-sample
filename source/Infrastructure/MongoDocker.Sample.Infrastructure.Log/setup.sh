1adc95b9522d92c52bfaacee2e241ee9aed777e5
rafaatsouza <souza.t.rafael@gmail.com>
2020-02-21 15:55:29 -0300

    Attempt to auth logseq

---
+++ source/Infrastructure/MongoDocker.Sample.Infrastructure.Log/setup.sh
@@ -0,0 +1,3 @@
+seqcli help
+# seqcli config -k connection.serverUrl -v https://logseq
+# seqcli config -k connection.serverUrl -v https://logseq && apikey create -t 'mongo-docker' --token=XQxfYTriAB0fJEXouJ4j
