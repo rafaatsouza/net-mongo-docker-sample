e1aaf04a95c2f1943c8f27b55987a6092653c8c1
rafaatsouza <souza.t.rafael@gmail.com>
2020-03-01 20:03:33 -0300

    Attemp to auth seq

---
+++ source/Infrastructure/MongoDocker.Sample.Infrastructure.Log/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo 'starting'
+
+SERVICE="http://logseq"
+
+until curl -fs $SERVICE  > /dev/null; do
+  >&2 echo "$SERVICE is unavailable - sleeping"
+  sleep 2
+done
+>&2 echo "$SERVICE is up"
+
+seqcli apikey create --title='newapikey' --token='123456' --server=$SERVICE
+
+exit 0
