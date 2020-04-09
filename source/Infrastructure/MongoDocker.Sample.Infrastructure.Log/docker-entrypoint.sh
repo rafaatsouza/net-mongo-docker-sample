377040170e91a75e6cd55aee85cb215feea921aa
Rafael Torres <souza.t.rafael@gmail.com>
2020-04-09 18:41:54 -0300

    Sets environments variables

--- source/Infrastructure/MongoDocker.Sample.Infrastructure.Log/docker-entrypoint.sh
+++ source/Infrastructure/MongoDocker.Sample.Infrastructure.Log/docker-entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh -e
 
-TOKEN=$1
-
-echo 'starting'
-echo "> ${TOKEN}"
-
 SERVICE="http://logseq"
 
 until curl -fs ${SERVICE}  > /dev/null; 
 do
   >&2 echo "${SERVICE} is unavailable - sleeping"
-  sleep 1
+  sleep 2
 done
 >&2 echo "${SERVICE} is up"
 
-/bin/seqcli/seqcli apikey create --title='mongo-docker-api' --token='${1}' --server=${SERVICE}
+#/bin/seqcli/seqcli apikey create --title='mongo-docker-api' --token='2(|=KcHS_N2u+TgV!%Fs7Z1fN$j_BI' --server=${SERVICE}
+/bin/seqcli/seqcli apikey create --title='mongo-docker-api' --token='${SEQ_LOG_TOKEN}' --server=${SERVICE}
 
 exit 0
