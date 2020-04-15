a5e73b42be65042afbd8685e77054fecdd78b8ce
rafaatsouza <souza.t.rafael@gmail.com>
2020-04-15 20:57:09 -0300

    Alters project arch into hexagonal

---
+++ source/Core/MongoDockerSample.Core.Domain/Exceptions/CustomException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace MongoDockerSample.Core.Domain.Exceptions
+{
+    public abstract class CustomException<T> : System.Exception where T : CustomError
+    {
+        public HttpStatusCode StatusCode { get; protected set; }
+
+        protected CustomException(CustomError error) : base(error.Message)
+        {
+            StatusCode = error.StatusCode;
+        }
+    }
+}
