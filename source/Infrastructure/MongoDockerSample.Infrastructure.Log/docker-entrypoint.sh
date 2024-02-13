cf82308e66c3eb1a5fa747cfff62302a82c4213b
Rafael Torres Souza <rafaatsouza@Rafaels-MacBook-Air-2.local>
2024-02-13 14:52:08 -0300

    change seq server url for script and bump image version

--- source/Infrastructure/MongoDockerSample.Infrastructure.Log/docker-entrypoint.sh
+++ source/Infrastructure/MongoDockerSample.Infrastructure.Log/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -e
 
-SERVICE="http://logseq"
-
-until curl -fs ${SERVICE}  > /dev/null; 
+until curl -fs ${SEQ_SERVER_URL}  > /dev/null;
 do
-  >&2 echo "${SERVICE} is unavailable - sleeping"
+  >&2 echo "${SEQ_SERVER_URL} is unavailable - sleeping"
   sleep 2
 done
->&2 echo "${SERVICE} is up"
+>&2 echo "${SEQ_SERVER_URL} is up"
 
-/bin/seqcli/seqcli apikey create --title='mongo-docker-api' --token=${SEQ_LOG_TOKEN} --server=${SERVICE}
+/bin/seqcli/seqcli apikey create --title='mongo-docker-api' --token=${SEQ_LOG_TOKEN} --server=${SEQ_SERVER_URL}
 
 exit 0
